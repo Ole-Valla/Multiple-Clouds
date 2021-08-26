7c90334b2dd5b565bff71f364c8c4a9348c795cc
Ole Jørgen Valla Dønnem <ole.jorgen.valla.donnem@nhn.no>
2021-08-26 12:32:02 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {
+default = "eu-west-1"}
+
+provider "aws" {
+access_key = "${var.aws_access_key}"
+secret_key = "${var.aws_secret_key}"
+region = "${var.aws_region}"
+}
+
+# aws amazon linux 
+resource "aws_instance" "web" {
+count ="2"
+ami = "ami-xxx"
+instance_type = "t3.micro"
+tags{
+name="webserver1"
+identity = "45343"
+location = "Glasgow"
+}
